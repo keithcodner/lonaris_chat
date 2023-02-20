d3883f7fc8c2974ccd25370e2ea2d834e5fa7866
keithcodner <codnerkj@gmail.com>
2023-02-20 02:52:21 -0500

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,25 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+
+const form = $('#form');
+const inputMassage = $('#input-message');
+
+form.addEventListener('submit', function(event){
+    const inputValue = inputMassage.val()
+
+    axios.post('/chat-message', {
+        message: inputValue
+    });
+})
+
+const channel = Echo.channel('public.playground.1');
+
+channel.subscribed( () => {
+    console.log('subscribed');
+}).listen('.playgroundzzz', (event) => {
+    console.log(event);
+});
+
 
 window.Alpine = Alpine;
 
